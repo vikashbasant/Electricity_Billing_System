0142381be6aa40b0a6b0e4325575228f4c47b74e
Vikash Basant <basantvikash360@gmail.com>
2023-01-20 11:30:03 +0530

    some change in viewInformation

--- src/electricity/ViewInformation.java
+++ src/electricity/ViewInformation.java
@@ -1,4 +1,3 @@
-
 package electricity;
 
 import javax.swing.*;
@@ -12,168 +11,234 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.logging.Logger;
 
-public class ViewInformation extends JFrame implements ActionListener{
+public class ViewInformation extends JFrame implements ActionListener {
 
     private static final Logger LOGGER = Logger.getLogger(String.valueOf(ViewInformation.class));
 
-    JButton b1;
+    JButton backButton;
 
-    ViewInformation(String meter){
+    ViewInformation (String meter) {
 
-        // set the bound:
-        setBounds(600,250, 850, 650);
+        //============================================================================================================//
+
+        /*----Create Frame----*/
+        setBounds(600, 250, 850, 665);
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
 
-        // Label VIEW CUSTOMER INFORMATION
+        //============================================================================================================//
+
+        /*----Label VIEW CUSTOMER INFORMATION----*/
         JLabel title = new JLabel("VIEW CUSTOMER INFORMATION");
         title.setBounds(250, 0, 500, 40);
-        title.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        title.setFont(new Font("Tahoma", Font.BOLD, 20));
+        // add title into frame:
         add(title);
 
-        // Label Name
-        JLabel l1 = new JLabel("Name");
-        l1.setBounds(70, 80, 100, 20);
-        add(l1);
 
-        // Label Name Value:
-        JLabel l11 = new JLabel();
-        l11.setBounds(250, 80, 100, 20);
-        add(l11);
+        //============================================================================================================//
 
-        // Label Meter Number:
-        JLabel l2 = new JLabel("Meter Number");
-        l2.setBounds(70, 140, 100, 20);
-        add(l2);
+        /*----Label Name----*/
+        JLabel lName = new JLabel("Name");
+        lName.setBounds(70, 80, 100, 20);
+        lName.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lName into frame:
+        add(lName);
 
-        // Label Meter Number Value:
-        JLabel l12 = new JLabel();
-        l12.setBounds(250, 140, 100, 20);
-        add(l12);
+        /*----Label Name Value:----*/
+        JLabel lNameVale = new JLabel();
+        lNameVale.setBounds(250, 80, 100, 20);
+        lNameVale.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lNameValue into frame:
+        add(lNameVale);
 
-        // Label Address:
-        JLabel l3 = new JLabel("Address");
-        l3.setBounds(70, 200, 100, 20);
-        add(l3);
+        //============================================================================================================//
 
-        // Label Address Value:
-        JLabel l13 = new JLabel();
-        l13.setBounds(250, 200, 100, 20);
-        add(l13);
+        /*----Label Meter Number:----*/
+        JLabel lMeterNo = new JLabel("Meter Number");
+        lMeterNo.setBounds(70, 140, 100, 20);
+        lMeterNo.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lMeterNo into frame:
+        add(lMeterNo);
 
-        // Label City:
-        JLabel l4 = new JLabel("City");
-        l4.setBounds(70, 260, 100, 20);
-        add(l4);
+        /*----Label Meter Number Value:----*/
+        JLabel lMeterValue = new JLabel();
+        lMeterValue.setBounds(250, 140, 100, 20);
+        lMeterValue.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lMeterValue into frame:
+        add(lMeterValue);
 
-        // Label City Value:
-        JLabel l14 = new JLabel();
-        l14.setBounds(250, 260, 100, 20);
-        add(l14);
+        //============================================================================================================//
 
-        // Label State:
-        JLabel l5 = new JLabel("State");
-        l5.setBounds(500, 80, 100, 20);
-        add(l5);
+        /*----Label Address:----*/
+        JLabel lAddress = new JLabel("Address");
+        lAddress.setBounds(70, 200, 100, 20);
+        lAddress.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lAddress into frame:
+        add(lAddress);
 
-        // Label State Value:
-        JLabel l15 = new JLabel();
-        l15.setBounds(650, 80, 100, 20);
-        add(l15);
+        /*----Label Address Value:----*/
+        JLabel lAddressValue = new JLabel();
+        lAddressValue.setBounds(250, 200, 100, 20);
+        lAddressValue.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lAddressValue into frame:
+        add(lAddressValue);
 
-        // Label Email:
-        JLabel l6 = new JLabel("Email");
-        l6.setBounds(500, 140, 100, 20);
-        add(l6);
+        //============================================================================================================//
 
-        // Label Email Value:
-        JLabel l16 = new JLabel();
-        l16.setBounds(650, 140, 150, 20);
-        add(l16);
+        /*----Label City:----*/
+        JLabel lCity = new JLabel("City");
+        lCity.setBounds(70, 260, 100, 20);
+        lCity.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lCity into frame:
+        add(lCity);
 
-        // Label Phone:
-        JLabel l7 = new JLabel("Phone");
-        l7.setBounds(500, 200, 100, 20);
-        add(l7);
+        /*----Label City Value:----*/
+        JLabel lCityValue = new JLabel();
+        lCityValue.setBounds(250, 260, 100, 20);
+        lCityValue.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lCityValue into frame:
+        add(lCityValue);
 
-        // Label Phone Value:
-        JLabel l17 = new JLabel();
-        l17.setBounds(650, 200, 100, 20);
-        add(l17);
-        
-        try{
+        //============================================================================================================//
+
+        /*----Label State:----*/
+        JLabel lState = new JLabel("State");
+        lState.setBounds(500, 80, 100, 20);
+        lState.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lState into frame
+        add(lState);
+
+        /*----Label State Value:----*/
+        JLabel lStateValue = new JLabel();
+        lStateValue.setBounds(650, 80, 100, 20);
+        lStateValue.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lStateValue into frame
+        add(lStateValue);
+
+        //============================================================================================================//
+
+        /*----Label Email:----*/
+        JLabel lEmail = new JLabel("Email");
+        lEmail.setBounds(500, 140, 100, 20);
+        lEmail.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lEmail into frame
+        add(lEmail);
+
+        /*----Label Email Value:----*/
+        JLabel lEmailValue = new JLabel();
+        lEmailValue.setBounds(650, 140, 150, 20);
+        lEmailValue.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lEmailValue into frame
+        add(lEmailValue);
+
+        //============================================================================================================//
+
+        /*----Label Phone:----*/
+        JLabel lPhone = new JLabel("Phone");
+        lPhone.setBounds(500, 200, 100, 20);
+        lPhone.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lPhone into frame
+        add(lPhone);
+
+        /*----Label Phone Value:----*/
+        JLabel lPhoneValue = new JLabel();
+        lPhoneValue.setBounds(650, 200, 100, 20);
+        lPhoneValue.setFont(new Font("Tahoma", Font.BOLD, 14));
+        // add lPhoneValue into frame:
+        add(lPhoneValue);
+
+        //============================================================================================================//
+
+        /*----Try to fetch all information of the specific meter no. and add into all values----*/
+
+        try {
 
             // Connection with database:
             Connection c = ConnectionProvider.getConnection();
             Statement s = c.createStatement();
 
+            String customerQuery = "select * from customer where meter = '" + meter + "'";
+            LOGGER.info("customerQuery: " + customerQuery);
+
             // Fetch the all records from customer table where meter = ?:
-            ResultSet rs = s.executeQuery("select * from customer where meter = '"+meter+"'");
+            ResultSet rs = s.executeQuery(customerQuery);
 
-            while(rs.next()){
+            while (rs.next()) {
 
-                // Fetch customer name from resultSet, set into l11
-                l11.setText(rs.getString(1));
+                // Fetch customer name from resultSet, set into lNameVale
+                lNameVale.setText(rs.getString(1));
 
-                // Fetch customer Meter Number from resultSet, set into l12
-                l12.setText(rs.getString(2));
+                // Fetch customer Meter Number from resultSet, set into lMeterValue
+                lMeterValue.setText(rs.getString(2));
 
-                // Fetch customer Address from resultSet, set into l13
-                l13.setText(rs.getString(3));
+                // Fetch customer Address from resultSet, set into lAddressValue
+                lAddressValue.setText(rs.getString(3));
 
-                // Fetch customer city from resultSet, set into l14
-                l14.setText(rs.getString(4));
+                // Fetch customer city from resultSet, set into lCityValue
+                lCityValue.setText(rs.getString(4));
 
-                // Fetch customer state from resultSet, set into l15
-                l15.setText(rs.getString(5));
+                // Fetch customer state from resultSet, set into lStateValue
+                lStateValue.setText(rs.getString(5));
 
-                // Fetch customer Email from resultSet, set into l16
-                l16.setText(rs.getString(6));
+                // Fetch customer Email from resultSet, set into lEmailValue
+                lEmailValue.setText(rs.getString(6));
 
-                // Fetch customer Phone from resultSet, set into l17
-                l17.setText(rs.getString(7));
-                
+                // Fetch customer Phone from resultSet, set into lPhoneValue
+                lPhoneValue.setText(rs.getString(7));
+
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
 
             e.printStackTrace();
             StringWriter errors = new StringWriter();
             e.printStackTrace(new PrintWriter(errors));
-            LOGGER.info("----ViewInformation:: Getting Exception ViewInformation Constructor----" + e.toString());
+            LOGGER.info("----ViewInformation:: Getting Exception ViewInformation Constructor----" + errors);
         }
 
-        // Create Back Button and set the properties into it:
-        b1 = new JButton("Back");
-        b1.setBackground(Color.BLACK);
-        b1.setForeground(Color.WHITE);
-        b1.setBounds(350, 340, 100, 25);
-        b1.addActionListener(this);
-        add(b1);
+        //============================================================================================================//
 
-        // Label ImageIcon:
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/viewcustomer.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(600, 300, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel l8  = new JLabel(i3);
-        l8.setBounds(20, 350, 600, 300);
-        add(l8);
+        /*----Create Back Button----*/
+        ImageIcon backIcon = new ImageIcon(ClassLoader.getSystemResource("icon/back3.png"));
+        Image backImage = backIcon.getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT);
+
+        backButton = new JButton("Back", new ImageIcon(backImage));
+        // set properties of Back Button:
+        backButton.setBackground(Color.WHITE);
+        backButton.setForeground(Color.BLACK);
+        backButton.setBounds(350, 340, 120, 30);
+        backButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        // addActionListener when click on this button then they trigger the action listener:
+        backButton.addActionListener(this);
+        // added Back Button to frame:
+        add(backButton);
+
+        //============================================================================================================//
+
+        /*----Label ImageIcon:----*/
+        ImageIcon iViewCustomer = new ImageIcon(ClassLoader.getSystemResource("icon/viewcustomer.jpg"));
+        Image imgViewCustomer = iViewCustomer.getImage().getScaledInstance(600, 300, Image.SCALE_DEFAULT);
+        JLabel viewCustomer = new JLabel(new ImageIcon(imgViewCustomer));
+        viewCustomer.setBounds(20, 350, 600, 300);
+        // add viewCustomer to frame:
+        add(viewCustomer);
 
     }
 
+    public static void main (String[] args) {
+
+        LOGGER.info("==: ViewInformation:: Inside main Method :==");
+        new ViewInformation("").setVisible(true);
+    }
+
     @Override
-    public void actionPerformed(ActionEvent ae){
+    public void actionPerformed (ActionEvent ae) {
 
         LOGGER.info("==: ViewInformation:: Inside actionPerformed Method:==");
 
         // simply close the current window:
         this.setVisible(false);
 
-    }
-    
-    public static void main(String[] args){
-
-        LOGGER.info("==: ViewInformation:: Inside main Method :==");
-        new ViewInformation("").setVisible(true);
     }
 }
